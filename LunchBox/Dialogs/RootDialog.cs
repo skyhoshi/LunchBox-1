5c2d0e66b53d64686d4207aa9e482274d2128b99
Steven Hook <steven@hookscode.com>
2018-04-05 20:11:30 -0400

    Simple message response decision

--- LunchBox/Dialogs/RootDialog.cs
+++ LunchBox/Dialogs/RootDialog.cs
@@ -18,12 +18,20 @@ namespace LunchBox.Dialogs
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
         {
             var activity = await result as Activity;
+            if (string.IsNullOrEmpty(activity.Text))
+            {
+                context.Wait(MessageReceivedAsync);
+                return;
+            }
 
-            // calculate something for us to return
-            int length = (activity.Text ?? string.Empty).Length;
-
-            // return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+            if (activity.Text.ToLower().Contains("hungry"))
+            {
+                await context.PostAsync("You're hungry? Ok, I can help you with that. Who is going to lunch with you?");
+            }
+            else
+            {
+                await context.PostAsync("Hey there! Let me know if you end up getting hungry.");
+            }
 
             context.Wait(MessageReceivedAsync);
         }
