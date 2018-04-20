5afaa46694959ad53ae4949384aa8db39fe4c7fe
Steven Hook <steven@hookscode.com>
2018-04-20 16:19:59 -0400

    End conversation after a recommendation

--- LunchBox/Dialogs/RootDialog.cs
+++ LunchBox/Dialogs/RootDialog.cs
@@ -68,6 +68,17 @@ namespace LunchBox.Dialogs
 
             message.Attachments.Add(card.ToAttachment());
             await context.PostAsync(message);
+
+            await context.PostAsync("I'll check back after lunch to see how everything went.");
+
+            await EndConversation(context);
+        }
+
+        private async Task EndConversation(IDialogContext context)
+        {
+            var message = context.MakeMessage();
+            message.Type = ActivityTypes.EndOfConversation;
+            await context.PostAsync(message);
         }
 
         private async Task RecommendationReceived(IDialogContext context, IAwaitable<Recommendation> result)
